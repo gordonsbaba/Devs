5850684cf60d9cf95b7c0932621994846a428098
g.m.okoronkwo@gmail.com <antzz@ANTZZ.LOCAL>
2025-02-07 16:08:41 +0000

    Second Commit

---
+++ edureka.py
@@ -0,0 +1,59 @@
+GIT COMMANDS
+git --version
+
+mkdir edureka-repo
+
+cd edureka-repo
+
+git init is to initialized the repository
+initialized empty Git repository in C:/Program Files/Git/edureka-repo/.git/
+
+
+
+
+I created a folder called Aba-folder and i created another folder called antzz-repro located on the Users>>antzz
+I further created a txt file called reading.txt
+Running git status shows that nothing has been committed yet( This means that any file created has to be committed
+in other to show up via local reprository
+
+to commit, first run git add reading.txt
+git commit -m "first commit"
+Use git diff when you make changes to your text file
+
+Note: To match github to git use the following command line
+git config --global user.name gordonsbaba-github
+
+git remote add origin git@github.com:gordonsbaba/Reading.git
+
+git push remote origin
+
+
+…or create a new repository on the command line
+echo "# reading" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin 
+git push -u origin main
+
+
+…or push an existing repository from the command line
+git remote add origin 
+git branch -M main
+git push -u origin main
+
++++++++++++++++++++++++
+To remove git origin and add a different origin use the below cmd
+git remote remove origin
+
+
+ssh-keygen -t ed25519 -C "g.m.okoronkwo@gmail.com"
+ssh-add "c:/Users/antzz/.ssh/id_ed25519"
+
+removing a directory
+use rm -rf name of dir
+
+git@github.com:gordonsbaba/Learning-DevOps.git
+
+
